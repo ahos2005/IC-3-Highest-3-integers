cdf585fd68b98caaa117f132b75251dfc936f233
Ahmed Elhosseiny <aelhosse@ucsd.edu>
2016-11-27 19:51:07 -0800

    Initial Commit

---
+++ IC-3-Highest-3-integers/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  IC-3-Highest-3-integers
+//
+//  Created by Il Dottore on 11/27/16.
+//  Copyright © 2016 Ahmed Elhosseiny. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
